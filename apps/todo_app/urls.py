6e15f4969accf37732c40e36904050512070b026
Shawn Nunoo <shawnnunoo@gmail.com>
2019-06-27 20:13:02 -0700

    added todo task function

--- apps/todo_app/urls.py
+++ apps/todo_app/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     url(r'^register$', views.addUser),
     url(r'^login$', views.loginUser, name='login'),
     url(r'^logout$', views.logout),
-    url(r'^home$', views.home),
+    url(r'^home/(?P<groupid>\d+)$', views.home),
     url(r'^register/group$', views.addGroup),
     url(r'^login/group$', views.loginGroup),
     url(r'^add_task$', views.add_task),
